2016d11d01541d4c9e8b6feee95239088a22befc
Cristian Jesus <cristian24jq@gmail.com>
2021-08-06 00:37:02 -0500

    restaurando views y templates

--- app/cmp/urls.py
+++ app/cmp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .import views
+from .import views 
+from.views import CompraDetDelete
 
 urlpatterns = [
     path('proveedor/',views.proveedorview,name='proveedor'),
@@ -7,5 +8,11 @@ urlpatterns = [
     path('editarProveedor/<int:id>',views.editarProveedor,name='editarProveedor'),
     path('deleteProveedor/<int:id>',views.deleteProveedor,name='delete'),
     path('inactivarProveedor/<int:id>',views.inactivarProveedor,name='inactivarProveedor'),
+
+    #compras 
+    path('compras/',views.Comprasview,name='compras_list'),
+    path('compras/new',views.compras,name='comprasNew'),
+    path('compras/edit/<int:compra_id>',views.compras,name='compras_edit'),
+    path('compras/<int:compra_id>/delete/<int:pk>',CompraDetDelete.as_view(),name='compras_del'),
     
 ]
