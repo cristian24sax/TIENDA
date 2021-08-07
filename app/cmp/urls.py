bd026f4dd4d379dd10934b301ec859bc49ffc3bf
Cristian Jesus <cristian24jq@gmail.com>
2021-08-07 00:24:27 -0500

    view.url,templates de reportes

--- app/cmp/urls.py
+++ app/cmp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .import views 
 from.views import CompraDetDelete
+from .reportes import reporte_compras,imprimir_compra
 
 urlpatterns = [
     path('proveedor/',views.proveedorview,name='proveedor'),
@@ -14,5 +15,7 @@ urlpatterns = [
     path('compras/new',views.compras,name='comprasNew'),
     path('compras/edit/<int:compra_id>',views.compras,name='compras_edit'),
     path('compras/<int:compra_id>/delete/<int:pk>',CompraDetDelete.as_view(),name='compras_del'),
-    
+    #html2pdf
+    path('compras/listado',reporte_compras,name='compras_print_all'),
+    path('compras/<int:compra_id>/imprimir',imprimir_compra,name='compras_print_one')
 ]
